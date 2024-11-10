3e65ebf8b023b26d94dd52eedf173dc73fca3e5b
Rafael Jose Tovar Garrido <rafaeltovar1992@gmail.com>
2024-11-10 16:11:21 +0100

    chore: fix format

--- python-app/app.py
+++ python-app/app.py
@@ -8,15 +8,16 @@ import json
 app = Flask(__name__)
 # Configure structlog to format log entries as JSON
 
-
-# Regex pattern to parse Werkzeug log lines
+# Regex pattern to parse Werkzeug log lines and to strip ANSI color codes
 log_pattern = re.compile(
     r'(?P<ip>[\d\.]+) - - \[(?P<datetime>[^\]]+)\] "(?P<method>\w+) (?P<url>[^ ]+) (?P<protocol>[^"]+)" (?P<status>\d+)'
 )
+ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')  # Matches ANSI color codes
 
 class JsonFormatter(logging.Formatter):
     def format(self, record):
-        log_message = record.getMessage()
+        # Strip ANSI escape codes
+        log_message = ansi_escape.sub('', record.getMessage())
         log_match = log_pattern.search(log_message)
 
         if log_match:
@@ -31,7 +32,7 @@ class JsonFormatter(logging.Formatter):
                 "status": log_match.group("status")
             }
         else:
-            # Fallback in case of non-matching log format
+            # Fallback if the message doesn't match the expected format
             log_record = {
                 "level": record.levelname,
                 "time": self.formatTime(record, self.datefmt),
@@ -57,6 +58,7 @@ werkzeug_logger.addHandler(werkzeug_handler)
 def hello_world():
     r = redis.Redis(host='redis-master.test.svc.cluster.local', port=6379, db=0)
     r.set('foo', 'bar')
+    app.logger.info("Home route accessed")
     return "Hello, World!"
 
 if __name__ == "__main__":
