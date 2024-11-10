8a4d385cc481f96588cceeb663b597e797d846f5
Rafael Jose Tovar Garrido <rafaeltovar1992@gmail.com>
2024-11-10 15:41:01 +0100

    chore: add python and redis communication

--- python-app/app.py
+++ python-app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    r = redis.Redis(host='redis-master.test.svc.cluster.local', port=6379, db=0)
+    r.set('foo', 'bar')
     return "Hello, World!"
 
 if __name__ == "__main__":
