636724c274c71cf459fb3ed227fb0aa7e6c6f0ba
Rafael Jose Tovar Garrido <rafaeltovar1992@gmail.com>
2024-11-12 20:44:43 +0100

    chore: add env

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -25,14 +25,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload 3.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button className="App-button" onClick={fetchGreeting}>
           Say Hi to the backend
         </button>
